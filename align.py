b55e90a0436642c1e36cacb1417ede3e8e708214
Stacey Gerben <srgerb@uw.edu>
2018-06-22 17:46:22 -0700

    rearranged folder

--- align.py
+++
@@ -1,6 +0,0 @@
-#Aligns chains from a pdb file individually
-#takes in 2 arguments, the base pdb to align to and the pdb to align
-#seperates chains and outputs as temp pdb's (base off of pdb2 name)
-#TMAlign each chain individually to base
-#get TMSup?
-#
